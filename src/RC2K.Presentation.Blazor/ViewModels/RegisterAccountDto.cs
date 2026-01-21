8c54c414f18fd024c9dc90f1cf098749a4f73c22
TheKetrab <ketrab508@gmail.com>
2026-01-21 11:12:17 +0100

    Razor refactor

--- src/RC2K.Presentation.Blazor/ViewModels/RegisterAccountDto.cs
+++ src/RC2K.Presentation.Blazor/ViewModels/RegisterAccountDto.cs
@@ -7,20 +7,20 @@ public class RegisterAccountDto
 {
     [Required(AllowEmptyStrings = false)]
     [StringLength(8, ErrorMessage = "Name length can't be more than 8.")]
-    public string? Username { get; set; }
+    public string Username { get; set; } = string.Empty;
 
     [Required(AllowEmptyStrings = false)]
     [EmailAddress]
-    public string? Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     public (string, string)? Nationality { get; set; }
 
-    [Required]
+    [Required(AllowEmptyStrings = false)]
     [StringLength(64, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
-    public string? Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 
-    [Required]
+    [Required(AllowEmptyStrings = false)]
     [Compare(nameof(Password))]
-    public string? Password2 { get; set; }
+    public string Password2 { get; set; } = string.Empty;
 
 }
