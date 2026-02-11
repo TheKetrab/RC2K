3c71cb72772b3c3815af2ba5a04456759603d003
Bartlomiej Grochowski <ketrab508@gmail.com>
2026-02-11 17:27:12 +0100

    Refactor: Initialize string properties to empty in Blazor components and update nullable types in ReCaptcha verifier

--- src/RC2K.Presentation.Shared/ViewModels/BreadcrumbModel.cs
+++ src/RC2K.Presentation.Shared/ViewModels/BreadcrumbModel.cs
@@ -1,10 +1,10 @@
-﻿
+
 namespace RC2K.Presentation.Shared.ViewModels;
 
 public class BreadcrumbLink
 {
     public int OrderIndex { get; set; }
-    public string Address { get; set; }
-    public string Title { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
     public bool IsActive { get; set; }
 }
