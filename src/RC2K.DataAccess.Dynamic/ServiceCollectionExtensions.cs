f24272b962d96aaeeca308adf98a4b87d967775f
TheKetrab <ketrab508@gmail.com>
2026-06-11 11:32:57 +0200

    Modularize DI registration

---
+++ src/RC2K.DataAccess.Dynamic/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RC2K.DataAccess.Dynamic.Mappers;
+using RC2K.DataAccess.Dynamic.Repositories;
+using RC2K.DataAccess.Interfaces;
+using RC2K.DataAccess.Interfaces.Repositories;
+using RC2K.DomainModel.Exceptions;
+
+namespace RC2K.DataAccess.Dynamic;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterDynamicDataAccess(
+        this IServiceCollection services, IConfiguration configuration, Type environmentProviderType)
+    {
+        var cosmosSection = configuration.GetSection("Cosmos");
+
+        string endpoint = cosmosSection["Endpoint"]
+            ?? throw new MissingConfigurationKeyException("Cosmos:Endpoint");
+        string database = cosmosSection["Database"]
+            ?? throw new MissingConfigurationKeyException("Cosmos:Database");
+        string primaryKey = cosmosSection["ApiKey"]
+            ?? throw new MissingConfigurationKeyException("Cosmos:ApiKey");
+        CosmosClient client =
+            new CosmosClientBuilder(endpoint, primaryKey)
+                .WithSystemTextJsonSerializerOptions(new System.Text.Json.JsonSerializerOptions())
+                .Build();
+
+        Database db = client.GetDatabase(database);
+        services.AddSingleton(db);
+
+        services.AddScoped<BonusPointsMapper>();
+        services.AddScoped<DriverMapper>();
+        services.AddScoped<NotificationMapper>();
+        services.AddScoped<TimeEntryMapper>();
+        services.AddScoped<RankingsMapper>();
+        services.AddScoped<UserMapper>();
+        services.AddScoped<VerifyInfoMapper>();
+
+        services.AddScoped(typeof(IEnvironmentProvider), environmentProviderType);
+
+        services.AddScoped<IBonusPointsRepository, BonusPointsRepository>();
+        services.AddScoped<IDriverRepository, DriverRepository>();
+        services.AddScoped<INotificationRepository, NotificationRepository>();
+        services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
+        services.AddScoped<IRankingsRepository, RankingsRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IVerifyInfoRepository, VerifyInfoRepository>();
+        
+        return services;
+    }
+}
---
+++ src/RC2K.Logic/ServiceCollectionExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RC2K.DomainModel.Exceptions;
+using RC2K.Logic.Fillers;
+using RC2K.Logic.Interfaces;
+using RC2K.Logic.Interfaces.Fillers;
+
+namespace RC2K.Logic;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterLogicServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // fillers
+        services.AddScoped<IBonusPointsFiller, BonusPointsFiller>();
+        services.AddScoped<ITimeEntryFiller, TimeEntryFiller>();
+        services.AddScoped<IDriverFiller, DriverFiller>();
+        services.AddScoped<IUserFiller, UserFiller>();
+        services.AddScoped<IVerifyInfoFiller, VerifyInfoFiller>();
+        services.AddScoped<IRankingFiller, RankingFiller>();
+        services.AddScoped<IFillersBag, FillersBag>();
+
+        // services
+        services.AddScoped<IBonusPointsService, BonusPointsService>();
+        services.AddScoped<IStageService, StageService>();
+        services.AddScoped<IDriverService, DriverService>();
+        services.AddScoped<IPointsProvider, PointsProvider>();
+        services.AddScoped<IPasswordProvider, PasswordProvider>(provider =>
+        {
+            var securitySection = configuration.GetSection("Security");
+            int iterations = securitySection.GetIntValue("Iterations")
+                ?? throw new MissingConfigurationKeyException("Security:Iterations");
+            string salt = securitySection["Salt"]
+                ?? throw new MissingConfigurationKeyException("Security:Salt");
+
+            return new PasswordProvider(iterations, salt);
+        });
+        services.AddScoped<ICarService, CarService>();
+        services.AddScoped<ITimeEntryService, TimeEntryService>();
+        services.AddScoped<INotificationService, NotificationService>();
+        services.AddScoped<IRankingService, RankingService>();
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+
+    private static int? GetIntValue(this IConfigurationSection configurationSection, string key)
+    {
+        if (int.TryParse(configurationSection[key], out int res))
+        {
+            return res;
+        }
+
+        return null;
+    }
+}
