b1db80847bb77b2083bff3f53c913b64f5b2ffa9
TheKetrab <ketrab508@gmail.com>
2026-06-11 12:33:10 +0200

    Setup integration tests

---
+++ tests/RC2K.IntegrationTests/DataAccess/Dynamic/CosmosRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using RC2K.DataAccess.Dynamic.EnvironmentProviders;
+using RC2K.DataAccess.Dynamic.Mappers;
+using RC2K.DataAccess.Dynamic.Repositories;
+using RC2K.DataAccess.Interfaces.Repositories;
+using RC2K.IntegrationTests;
+
+namespace RC2K.IntegrationTests.DataAccess.Dynamic;
+
+public class CosmosRepositoryTests
+{
+    private ITimeEntryRepository _timeEntryRepository;
+
+    [SetUp]
+    public void Setup()
+    {
+        _timeEntryRepository = IntegrationFixture.ServiceProvider.GetRequiredService<ITimeEntryRepository>();
+    }
+
+    [Test]
+    public async Task Scenario1_ContainerIsAccessible()
+    {
+        var res = await _timeEntryRepository.GetByStageId(1, CancellationToken.None);
+        ;
+    }
+}
---
+++ tests/RC2K.IntegrationTests/IntegrationFixture.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RC2K.DataAccess.Database;
+using RC2K.DataAccess.Dynamic;
+using RC2K.DataAccess.Dynamic.EnvironmentProviders;
+using RC2K.Logic;
+
+namespace RC2K.IntegrationTests;
+
+[SetUpFixture]
+public class IntegrationFixture
+{
+    private static IServiceProvider? _serviceProvider;
+    public static IServiceProvider ServiceProvider => _serviceProvider ?? throw new Exception();
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+
+        try
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<IntegrationFixture>()
+                .Build();
+
+            IServiceCollection services = new ServiceCollection()
+                .RegisterPersistentDataAccess(s => { })
+                .RegisterDynamicDataAccess(configuration, typeof(DevEnvironmentProvider))
+                .RegisterLogicServices(configuration);
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+        catch (Exception ex)
+        {
+            
+        }
+
+        
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        if (_serviceProvider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
