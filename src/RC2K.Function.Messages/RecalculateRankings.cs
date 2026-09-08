a4b87c6a7613ae155ba94f25c7168932a5d87eff
TheKetrab <ketrab508@gmail.com>
2026-09-08 13:30:07 +0200

    Recalculate rankings implementation

--- src/RC2K.Function.Messages/RecalculateRankings.cs
+++ src/RC2K.Function.Messages/RecalculateRankings.cs
@@ -1,25 +1,47 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.DurableTask;
-using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using RC2K.DomainModel;
-using RC2K.Logic.Interfaces;
-using System.Text;
-using System.Text.Json;
+using RC2K.DomainModel.Exceptions;
 
 namespace RC2K.Function.Messages;
 
+public class Rc2kHubRankingsHttpClient(
+    HttpClient httpClient,
+    ILogger<Rc2kHubRankingsHttpClient> logger)
+{
+    public async Task SendPostRankingAutoSnapshotMessage(string credentials)
+    {
+        using HttpRequestMessage request = new(HttpMethod.Post, "ranking/auto-snapshot");
+        request.Headers.TryAddWithoutValidation("Authorization", $"Basic {credentials}");
+        request.Content = null;
+
+        try
+        {
+            using var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+        }
+    }
+}
+
 public class RecalculateRankings(
-    ILogger<SendDiscordMessage> logger)
+    IConfiguration configuration,
+    Rc2kHubRankingsHttpClient rc2khubRankingsHttpClient)
 {
     /// <summary>
     /// Triggered every Monday at 6:00 UTC (8:00 CEST, 7:00 CET - pl time)
     /// </summary>
     [Function(nameof(RecalculateRankings_Job))]
-    public Task RecalculateRankings_Job(
+    public async Task RecalculateRankings_Job(
         [TimerTrigger("0 0 6 * * 1")] TimerInfo myTimer)
     {
-        // TODO
-        return Task.CompletedTask;
+        var appClientSection = configuration.GetSection("AppClient");
+        string credentials = appClientSection["AzureFunctionsBotCredentials"]
+            ?? throw new MissingConfigurationKeyException("AppClient:AzureFunctionsBotCredentials");
+
+        await rc2khubRankingsHttpClient.SendPostRankingAutoSnapshotMessage(credentials);
     }
 }
